f5710f84bff23d63a86f9218f458ca9b4c17f366
KalykovAJ <aj.kalyk@gmail.com>
2024-04-28 19:25:54 +0600

    feat: Created components for svg images and displayed them on the login page

--- src/Inputs/Inputs.jsx
+++ src/Inputs/Inputs.jsx
@@ -1,50 +1,52 @@
-import { useState } from "react";
-import "./inputs.css"
+import { useState } from 'react'
+import './inputs.css'
 
+import { IoEyeOutline } from 'react-icons/io5'
 
-import { IoEyeOutline } from "react-icons/io5";
+export default function Inputs() {
+	const [on, setOff] = useState(false)
 
+	const sw = () => {
+		if (on == false) {
+			return setOff(true)
+		} else if (on == true) {
+			return setOff(false)
+		}
+	}
+	return (
+		<>
+			<div className='inputs'>
+				<div className='TI'>
+					<p className='textName text'>Имя</p>
+					<input type='text' className='inputName input' />
+				</div>
+				<div className='TI'>
+					<p className='textEmail text'>Email</p>
+					<input type='text' className='inputEmail input' />
+				</div>
+				<div className='TI'>
+					<p className='textPassword text'>Пароль</p>
+					<input
+						type={on == true ? `text` : `password`}
+						className='inputPassword input'
+					/>
+					<IoEyeOutline onClick={sw} size={20} className='iconEye' />
+				</div>
 
-export default function Inputs (){
-   
-   const [on, setOff] = useState(false)
-   
-   const sw = ()=>{
-      if (on == false){
-         return setOff(true)
-      }else if(on == true){
-         return setOff(false)
-      }
-
-   }
-   return (
-      <>
-         <div className="inputs">
-            <div className="TI">
-               <p className="textName text">Имя</p>
-               <input type="text" className="inputName input"/>
-            </div>
-            <div className="TI">
-               <p className="textEmail text">Email</p>
-               <input type="text"  className="inputEmail input"/>
-            </div>
-            <div className="TI">
-               <p className="textPassword text">Пароль</p>
-               <input type={on == true? `text` : `password`} className="inputPassword input" />
-               <IoEyeOutline onClick={sw} size={20} className="iconEye"/>
-            </div>
-
-
-
-            <div className="CHB">
-               <div className="divCheckbox">
-                  <input type="checkbox" name="" id="" className="checkbox"/>
-                  <p className="condition">Я принимаю условие соглашение </p>
-               </div>
-               <div className="enter">Войти</div>
-            </div>
-               <p className="linkOfLogin">у вас есть аккаунт?<a href="#" className="link">ПЕРЕХОДИТЕ ПРЯМО СЕЙЧАС</a></p>
-         </div>
-      </>
-   )
+				<div className='CHB'>
+					<div className='divCheckbox'>
+						<input type='checkbox' name='' id='' className='checkbox' />
+						<p className='condition'>Я принимаю условие соглашение </p>
+					</div>
+					<div className='enter'>Войти</div>
+				</div>
+				<p className='linkOfLogin'>
+					у вас есть аккаунт?
+					<a href='#' className='link'>
+						ПЕРЕХОДИТЕ ПРЯМО СЕЙЧАС
+					</a>
+				</p>
+			</div>
+		</>
+	)
 }
--- src/LeftSide/LeftSide.jsx
+++ src/LeftSide/LeftSide.jsx
@@ -1,20 +1,32 @@
+import CommunityImages from '../components/svg/CommunityImages'
+import Logo from '../components/svg/Logo'
 import './leftSide.css'
 
-export default function LeftSide(){
-
-   const img = ""
-   return(
-      <>
-         <div className="leftSide">
-            <div>
-               
-            </div>
-            <div className='textLeftSide'>
-               <p className='headerInLeftSide'> <p className='first'>Присоединяйся к</p> <p className='second'> онлайн сообществу!!!</p></p>
-               <p5 className='discription'> <p className='fristPart'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum,</p> <p className='secondPart'>
-                  Quasi culpa laborum dolorum deleniti</p></p5>
-            </div>
-         </div>
-      </>
-   )
+export default function LeftSide() {
+	// const img = ''
+	return (
+		<>
+			<div className='leftSide'>
+				<div className='boxLogoAndImage'>
+					<Logo />
+					<CommunityImages />
+				</div>
+				<div className='textLeftSide'>
+					<div className='headerInLeftSide'>
+						{' '}
+						<p className='first'>Присоединяйся к</p>{' '}
+						<p className='second'> онлайн сообществу!!!</p>
+					</div>
+					<div className='discription'>
+						{' '}
+						<p className='fristPart'>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+							ipsum,
+						</p>{' '}
+						<p className='secondPart'>Quasi culpa laborum dolorum deleniti</p>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
